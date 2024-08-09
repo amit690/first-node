976755d5eab73418df8a49bd12557aa48cc6985d
amit690 <amitsanyal191@gmail.com>
2024-08-09 12:30:25 +0530

    second assingment

---
+++ nodeAssignment2.js
@@ -0,0 +1,10 @@
+const mul=(num1,num2)=> num1*num2;
+
+const student={
+    Name: "amit",
+    Roll: 34,
+    mulltiplay: (a,b) => a*b
+}
+console.log(mul(9,7));
+console.log(student.mulltiplay(3,4));
+console.log(student);
